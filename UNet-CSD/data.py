6de7fedd45925e71e361904bc33370f80ca45e28
yuto_kizawa <yuto17qc@gmail.com>
2023-10-13 22:57:13 +0900

    a

--- UNet-CSD/data.py
+++ UNet-CSD/data.py
@@ -2,9 +2,18 @@ import pandas as pd
 
 """
 
-/home/yuto_kizawa/Project/Research/UNet-CSD/train/case2/case2_day1/scans/
-slice_0001_266_266_1.50_1.50.png
+元画像path, ラベルpath を要素とするcsv 「path.csv」 を作成
+
+/home/yuto_kizawa/Project/Research/UNet-CSD/train/case123/case123_day20/scans/
+slice_0065_266_266_1.50_1.50.png
+
+case123_day20_slice_0065    ←この3つのindexで一意に指定可能
 
 """
 
 df = pd.read_csv("/home/yuto_kizawa/Project/Research/UNet-CSD/with_label.csv")
+
+# imgpath labelpath
+
+
+
