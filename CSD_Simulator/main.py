33da073fcd49f67754cbde2fd8c86ecc6dbca1e7
yuto_kizawa <yuto17qc@gmail.com>
2023-10-22 14:35:19 +0900

    DQD parameter の特徴

--- CSD_Simulator/main.py
+++ CSD_Simulator/main.py
@@ -6,14 +6,23 @@ from simulation.double_dot import DoubleQuantumDot
 
 def main() -> None:
 
-    # parameter
-    c_01 = -0.1
-    c_gate0_0 = -0.8
-    c_gate0_1 = -0.1
-    c_gate1_0 = -0.1
-    c_gate1_1 = -0.8
+    # DQD parameter
+    c_01 = -0.1                             # 構造              ~ -5.0 ~ -0.05 ~ 
+    c_gate0_0 = -0.8                        # 拡大縮小          -0.8 ~
+    c_gate0_1 = -0.2                        # 傾き              -0.2 ~ -0.001
+    c_gate1_0 = -0.2                        # 傾き              -0.2 ~ -0.001
+    c_gate1_1 = -0.8                        # 拡大縮小          -0.8 ~
     c_0 = -(c_01 + c_gate0_0 + c_gate1_0)
     c_1 = -(c_01 + c_gate0_1 + c_gate1_1)
+    e = 2.0                                 # 拡大縮小          2.0~
+    v_s = 0.0                               # 線形 / 非線形
+
+    # CSD parameter 
+    thickness = 0.05
+    pepper_prob = 0.2
+    salt_prob = 0.0
+    random_prob = 0.0
+    gaussian = 1.0
 
     # range of v0 / v1
     range_v0 = np.arange(0, 10, 0.1) #元0, 10, 0.05
@@ -29,16 +38,19 @@ def main() -> None:
         c_gate0_1=c_gate0_1,
         c_gate1_0=c_gate1_0,
         c_gate1_1=c_gate1_1,
-        e=7,
-        v_s=0.0,
+        e=e,
+        v_s=v_s,
     )
 
+    # CSD
     original_csd, noisy_csd = dqd.simulation_CSD(
         range_v0=range_v0, 
         range_v1=range_v1, 
-        thickness=0.05, 
-        pepper=0.2,
-        gaussian=1.0, 
+        thickness=thickness, 
+        salt_prob=salt_prob,
+        pepper_prob=pepper_prob,
+        random_prob=random_prob,
+        gaussian=gaussian, 
     )
 
     # 描画
