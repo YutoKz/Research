a4df7b8ad140a2a1d95441aa649cd31a294ab072
yuto_kizawa <yuto17qc@gmail.com>
2023-10-27 14:28:17 +0900

    正方形以外の画像に対応

--- CSD_Analyzer/src/infer.py
+++ CSD_Analyzer/src/infer.py
@@ -21,10 +21,12 @@ model.load_state_dict(torch.load("./models/train_5.pth"))
 #model.eval()
 sigmoid = nn.Sigmoid()
 
-img = cv2.imread("./data/takahashi/1_1_csd_paper1_remove_text_gray.png")
+img = cv2.imread("./data/takahashi/csd_paper1_remove_text_gray.png")
 #img = cv2.resize(img, dsize = (144, 144))   #応急処置　元々2のべき乗とかならいらん
-required_size = [i for i in range(img.shape[0] - 15, img.shape[0] + 1) if i % 16 == 0]
-img = cv2.resize(img, dsize = (required_size[0], required_size[0]))
+required_height = [i for i in range(img.shape[0] - 15, img.shape[0] + 1) if i % 16 == 0]
+required_width = [i for i in range(img.shape[1] - 15, img.shape[1] + 1) if i % 16 == 0]
+print(f"{required_height},{required_width}")
+img = cv2.resize(img, dsize = (required_width[0], required_height[0]))
 img = img/255
 img = torch.from_numpy(img.astype(np.float32)).clone()
 img = img.permute(2, 0, 1)
