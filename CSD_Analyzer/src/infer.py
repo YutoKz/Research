87fa3cc7364818189796a932ca332aab7a89359f
yuto_kizawa <yuto17qc@gmail.com>
2023-10-27 14:31:14 +0900

    minor

--- CSD_Analyzer/src/infer.py
+++ CSD_Analyzer/src/infer.py
@@ -25,7 +25,7 @@ img = cv2.imread("./data/takahashi/csd_paper1_remove_text_gray.png")
 #img = cv2.resize(img, dsize = (144, 144))   #応急処置　元々2のべき乗とかならいらん
 required_height = [i for i in range(img.shape[0] - 15, img.shape[0] + 1) if i % 16 == 0]
 required_width = [i for i in range(img.shape[1] - 15, img.shape[1] + 1) if i % 16 == 0]
-print(f"{required_height},{required_width}")
+print(f"height:{required_height}, width:{required_width}")
 img = cv2.resize(img, dsize = (required_width[0], required_height[0]))
 img = img/255
 img = torch.from_numpy(img.astype(np.float32)).clone()
