9585cfd474fe50b971b5c4a7640f5e75fe760a58
yuto_kizawa <yuto17qc@gmail.com>
2023-11-24 22:20:48 +0900

    minor

--- CSD_Analyzer/src/infer.py
+++ CSD_Analyzer/src/infer.py
@@ -17,18 +17,21 @@ unet = UNet_2D().to(device)
 
 # test
 model = UNet_2D()
-model.load_state_dict(torch.load("./models/train_3.pth"))
+model.load_state_dict(torch.load("./models/train_29.pth"))
 #model.eval()
 sigmoid = nn.Sigmoid()
 
-#img = cv2.imread("./data/takahashi/big_csd_paper1_remove_text_gray.png")
-#img = cv2.resize(img, dsize = (192, 192))   #応急処置　元々2のべき乗とかならいらん
-img = cv2.imread("./data/csd.png")
+img_orig = cv2.imread("./data/takahashi/big_csd_paper1_remove_text_gray.png")
+#img_orig = cv2.imread("./data/csd.png")
 
-required_height = [i for i in range(img.shape[0] - 15, img.shape[0] + 1) if i % 16 == 0]
-required_width = [i for i in range(img.shape[1] - 15, img.shape[1] + 1) if i % 16 == 0]
+required_height = [i for i in range(img_orig.shape[0] - 15, img_orig.shape[0] + 1) if i % 16 == 0]
+required_width = [i for i in range(img_orig.shape[1] - 15, img_orig.shape[1] + 1) if i % 16 == 0]
 print(f"height:{required_height}, width:{required_width}")
-img = cv2.resize(img, dsize = (required_width[0], required_height[0]))
+img = cv2.resize(img_orig, dsize = (required_width[0], required_height[0]))
+
+# サイズを手動で変更したい場合に使用。普段はコメントアウト
+img = cv2.resize(img_orig, dsize=(192, 192))
+
 img = img/255
 img = torch.from_numpy(img.astype(np.float32)).clone()
 img = img.permute(2, 0, 1)
