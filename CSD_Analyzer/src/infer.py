8b50e91fdd0bec93b5cc86b5119f3f1e2211a652
yuto_kizawa <yuto17qc@gmail.com>
2023-11-29 22:03:53 +0900

    feat: infer.py 分類クラス数変更可能に。

--- CSD_Analyzer/src/infer.py
+++ CSD_Analyzer/src/infer.py
@@ -8,11 +8,18 @@ import torch.nn as nn
 #from PIL import Image
 
 import os
+import shutil
 
 from model import UNet_2D
 
+# フォルダ準備
+if os.path.exists("./data/output_infer"):
+  shutil.rmtree("./data/output_infer")
+
+os.mkdir("./data/output_infer") 
+
 # 分類するクラス数
-classes = 2
+classes = 3
 
 # GPU, Optimizer, Loss function
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -20,15 +27,16 @@ unet = UNet_2D(classes=classes).to(device)
 
 # test
 model = UNet_2D(classes=classes)
-model.load_state_dict(torch.load("./models/train_29.pth"))
+model.load_state_dict(torch.load("./models/train_23.pth"))
+
 #model.eval()
 sigmoid = nn.Sigmoid()
 
-img_orig = cv2.imread("./data/input_takahashi/big_csd_paper1_remove_text_gray.png")
+img_orig = cv2.imread("./data/input_hitachi/original/csd100.png")
 
 required_height = [i for i in range(img_orig.shape[0] - 15, img_orig.shape[0] + 1) if i % 16 == 0]
 required_width = [i for i in range(img_orig.shape[1] - 15, img_orig.shape[1] + 1) if i % 16 == 0]
-print(f"height:{required_height}, width:{required_width}")
+print(f"Output size: ({required_height[0]}, {required_width[0]})")
 img = cv2.resize(img_orig, dsize = (required_width[0], required_height[0]))
 
 # サイズを手動で変更したい場合に使用。普段はコメントアウト
@@ -41,11 +49,15 @@ img = img.unsqueeze(0)
 
 output = model(img)
 output = sigmoid(output)
-pred = torch.argmax(output, axis=1)
-pred = torch.nn.functional.one_hot(pred.long(), num_classes=2).to(torch.float32)
+pred = torch.argmax(output, dim=1)
+pred = torch.nn.functional.one_hot(pred.long(), num_classes=classes).to(torch.float32)
 
 orig = img[0,0,:,:].cpu().numpy()
 cv2.imwrite("./data/output_infer/original.png", orig*255)
 
-pred_np = pred[0,:,:,1].cpu().numpy()
-cv2.imwrite("./data/output_infer/pred.png", pred_np*255)
+pred_np = torch.argmax(pred[0,:,:,:], dim=2).cpu().numpy()
+cv2.imwrite(f"./data/output_infer/pred.png", pred_np*255//(classes-1))
+for j in range(classes):
+      if j != 0:
+        pred_np = pred[0,:,:,j].cpu().numpy()
+        cv2.imwrite(f"./data/output_infer/class{j}.png", pred_np*255)
