25f890e59509b5bbcbfa0163a84c41c9545f13cd
yuto_kizawa <yuto17qc@gmail.com>
2023-12-01 21:47:57 +0900

    add: 三角形の輪郭をエッジ検出、直線画像と統合

---
+++ CSD_Analyzer/src/hough/utils.py
@@ -0,0 +1,25 @@
+import cv2
+
+def integrate_edges(
+    filepath_line: str,
+    filepath_triangle: str,
+) -> str:
+    """三角形の輪郭と直線を合わせる
+
+    Args:
+        filepath_line: CSDの直線の二値画像
+        filepath_triangle: CSDの三角形の二値画像
+        
+    """
+    filepath_output = "./data/output_utils/edge.png"
+
+    img_line = cv2.imread(filepath_line, cv2.IMREAD_GRAYSCALE)
+    img_triangle = cv2.imread(filepath_triangle, cv2.IMREAD_GRAYSCALE)
+
+    edges_triangle = cv2.Canny(img_triangle, 50, 100)
+
+    output = cv2.add(img_line, edges_triangle)
+
+    cv2.imwrite(filepath_output, output)
+
+    return filepath_output
