d522bb7e1e4a745746b5a426b975e2caefcb97b3
yuto_kizawa <yuto17qc@gmail.com>
2023-12-02 14:35:07 +0900

    feat: interceptを電圧単位で出力。 傾きを原点左下として出力。 csvに直線のタイプを追加。

--- CSD_Analyzer/src/hough/hough.py
+++ CSD_Analyzer/src/hough/hough.py
@@ -5,6 +5,7 @@ import os
 
 from utils import integrate_edges
 
+# あとからdetect_line_segmentのみ改良していったので、こっちを使う場合は注意
 def detect_line(
     filepath: str,
     edge_extraction: bool = True,
@@ -57,6 +58,7 @@ def detect_line(
 
 def detect_line_segment(
     filepath: str,
+    voltage_per_pixel: float = 1.0,
     edge_extraction: bool = True,
     hough_threshold: int = 10,
     hough_minLineLength: int = 10,
@@ -70,6 +72,8 @@ def detect_line_segment(
     image = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
     rgb_image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
     cv2.imwrite("./data/output_hough/original.png", image)
+    height, width = image.shape[:2]
+    print(f"image size: ({height}, {width})")
 
     # ハフ変換による直線検出
     if edge_extraction:
@@ -89,27 +93,34 @@ def detect_line_segment(
         x1, y1, x2, y2 = line[0]
         
         # 直線の切片と傾き
-        slope = (y2 - y1) / (x2 - x1 + 1e-6)  # ゼロ除算を防ぐために小さな値を追加
-        intercept = y1 - slope * x1
-        lines_list.append([slope, intercept])
+        # cv2では左上が原点, 横軸x縦軸yであることに注意
+        slope_minus = (y2 - y1) / (x2 - x1 + 1e-6)  # ゼロ除算を防ぐ
+        slope = slope_minus * -1
+        intercept = (height - y1) - slope * x1
 
         # 直線を描画
-        if slope > 1:
+        if slope < -1:
+            # 赤
             line_color = (0, 0, 255)
             num_of_vertical_lines += 1
-        elif 0 <= slope <= 1:
+            lines_list.append(["vertical", slope, intercept * voltage_per_pixel])
+        elif -1 <= slope <= 0:
+            # 緑
             line_color = (0, 255, 0)
             num_of_horizontal_lines += 1
+            lines_list.append(["horizontal",slope, intercept * voltage_per_pixel])
         else:
+            # 青
             line_color = (255, 0, 0)
             num_of_interdot_lines += 1
+            lines_list.append(["interdot", slope, intercept * voltage_per_pixel])
         cv2.line(rgb_image, (x1, y1), (x2, y2), line_color, 1)
     print(f"|- Horizontal: {num_of_horizontal_lines}\n|- Vertical:   {num_of_vertical_lines}\n|- Interdot:   {num_of_interdot_lines}")
 
     # 結果の保存
     cv2.imwrite("./data/output_hough/detected_lines.png", rgb_image)
-    df = pd.DataFrame(lines_list, columns=["slope", "intercept"])
-    df.to_csv("./data/output_hough/line_parapeters.csv", index=False)
+    df = pd.DataFrame(lines_list, columns=["type", "slope", "intercept"])
+    df.sort_values(by="type").to_csv("./data/output_hough/line_parapeters.csv", index=False)
 
 if __name__ == "__main__":
     #filepath_line = "./data/output_infer/class1.png"
@@ -121,8 +132,8 @@ if __name__ == "__main__":
     detect_line_segment(
         filepath=filepath, 
         edge_extraction=False,
-        hough_threshold=30,         # 10
-        hough_minLineLength=10,     # 10
-        hough_maxLineGap=2          # 8
+        hough_threshold=30,         
+        hough_minLineLength=10,     
+        hough_maxLineGap=2              
     )
     
