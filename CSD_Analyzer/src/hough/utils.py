94c0d57363820ac14709333dbdc27e14c0a73ba1
yuto_kizawa <yuto17qc@gmail.com>
2023-12-07 17:45:11 +0900

    細線化関数修正。完成

--- CSD_Analyzer/src/hough/utils.py
+++ CSD_Analyzer/src/hough/utils.py
@@ -1,7 +1,14 @@
 import cv2
 import numpy.typing as npt
+from skimage import io, color
+from skimage.morphology import skeletonize
+import os, shutil
 
 output_folder = "./data/output_utils"
+
+if os.path.exists(output_folder):
+    shutil.rmtree(output_folder)
+os.mkdir(output_folder)
 
 def integrate_edges(
     filepath_line: str,
@@ -28,8 +35,22 @@ def integrate_edges(
     return filepath_output
 
 def thin_binary_image(filepath: str) -> npt.NDArray:
+    """
     binary_image = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
     thinning = cv2.ximgproc.thinningGuoHall(binary_image)
     cv2.imwrite(output_folder + "/thinning.png", thinning)
     
     return thinning
+    """
+    binary_image = io.imread(filepath, as_gray=True)
+    thinned_image = skeletonize(binary_image)
+    thinned_image = (thinned_image * 255).astype('uint8')
+    cv2.imwrite(output_folder + "/original.png", binary_image)
+    cv2.imwrite(output_folder + "/thinning.png", thinned_image)
+
+    return thinned_image
+
+if __name__ == "__main__":
+    filepath = "./data/_archive/takahashi/192_192.png"
+
+    thin_binary_image(filepath=filepath)
